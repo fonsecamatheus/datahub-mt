8ef02bdfcd22c067e91fe7caab3a3ad2149d95ed
mths-fonseca <matheusrdf1997@protonmail.com>
2024-09-19 21:11:27 -0400

    implementando config

---
+++ source/config/config.py
@@ -0,0 +1,24 @@
+config = [
+    {
+        'pesquisa': 'Produção Agrícola Municipal (PAM)',
+        'tabelas': [
+            {'id_tabela': 5457,
+             'nome_tabela': 'Área plantada ou destinada à colheita, área colhida, quantidade produzida, rendimento médio e valor da produção das lavouras temporárias e permanentes',
+             'variaveis': [{'id_variavel': 8331, 'nome_variavel': 'Área plantada ou destinada à colheita'}, 
+                           {'id_variavel': 216, 'nome_variavel': 'Área colhida'},
+                           {'id_variavel': 214, 'nome_variavel': 'Quantidade produzida'}]}
+        ]
+    },
+    {
+        'pesquisa': 'Produção da Extração Vegetal e da Silvicultura (PEVS)',
+        'tabelas': [
+            {'id_tabela': 289,
+             'nome_tabela': 'Quantidade produzida e valor da produção na extração vegetal, por tipo de produto extrativo',
+             'variaveis': [{'id_variavel': 144, 'nome_variavel': 'Quantidade produzida na extração vegetal'}, 
+                           {'id_variavel': 145, 'nome_variavel': 'Valor da produção na extração vegetal'}]},
+            {'id_tabela': 291,
+             'nome_tabela': 'Quantidade produzida e valor da produção na silvicultura, por tipo de produto da silvicultura',
+             'variaveis': [{'id_variavel': 142, 'nome_variavel': 'Quantidade produzida na silvicultura'}, 
+                           {'id_variavel': 143, 'nome_variavel': 'Valor da produção na silvicultura'}]}]
+    }
+]
